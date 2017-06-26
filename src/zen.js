fe64be376f529ef671438a2b03c9fb10ab60e5ec
psfrankie <frankie-orourke@pluralsight.com>
2017-06-26 12:09:02 -0500

    clean up function

--- src/zen.js
+++ src/zen.js
@@ -4,12 +4,12 @@ const {
   failure,
   isFailure
 } = require('effects-as-data/node');
+const FIVE_SECONDS = 1000 * 5;
 
 function* zen() {
-  const fiveSeconds = 1000 * 5;
   const cacheActions = [actions.now(), actions.getState(['zen', 'time'])];
   const [$now, $state] = yield cacheActions;
-  const cacheIsFresh = $now.payload - $state.payload.time < fiveSeconds;
+  const cacheIsFresh = $now.payload - $state.payload.time < FIVE_SECONDS;
   if ($state.payload.zen && cacheIsFresh) return $state.payload.zen;
   const $zen = yield actions.httpGet('https://api.github.com/zen');
   if (isFailure($zen)) return $zen;
