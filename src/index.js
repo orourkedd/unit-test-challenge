14a3b4b4235ae4360a9cd6e785dbaf0809204bdc
psfrankie <frankie-orourke@pluralsight.com>
2017-06-26 12:22:00 -0500

    cleaning up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const { handlers } = require('effects-as-data/node');
-const { zen } = require('./zen');
-const { run } = require('effects-as-data');
+const { handlers } = require('effects-as-data/node')
+const zen = require('./zen')
+const { run } = require('effects-as-data')
 
-const runIt = () => run(handlers, zen).then(console.log);
+const runIt = () => run(handlers, zen).then(console.log)
 
-runIt();
-
-setTimeout(() => {
-  console.log('\nAfter 2 seconds:');
-  runIt();
-}, 2000);
+runIt()
 
 setTimeout(() => {
-  console.log('\nAfter 4 seconds:');
-  runIt();
-}, 4000);
+  console.log('\nAfter 2 seconds:')
+  runIt()
+}, 2000)
 
 setTimeout(() => {
-  console.log('\nAfter 8 seconds (should be new wisdom):');
-  runIt();
-}, 8000);
+  console.log('\nAfter 4 seconds:')
+  runIt()
+}, 4000)
+
+setTimeout(() => {
+  console.log('\nAfter 8 seconds (should be new wisdom):')
+  runIt()
+}, 8000)
--- src/zen.spec.js
+++ src/zen.spec.js
@@ -1,13 +1,13 @@
-const { zen } = require('./zen');
-const { actions, success, failure } = require('effects-as-data/node');
-const { testIt } = require('effects-as-data/test');
+const zen = require('./zen')
+const { actions, success, failure } = require('effects-as-data/node')
+const { testIt } = require('effects-as-data/test')
 
-const testZen = testIt(zen);
+const testZen = testIt(zen)
 
 test(
   'zen() should cache miss if cache is empty, GET zen from github, set cache, and return result',
   testZen(() => {
-    const now = 1492180435000;
+    const now = 1492180435000
 
     // prettier-ignore
     return [
@@ -17,13 +17,13 @@ test(
       [null, 'foo']
     ]
   })
-);
+)
 
 test(
   'zen() should return zen from the cache if the cache is less than 5 seconds old',
   testZen(() => {
-    const cacheTime = 1492180435000;
-    const now = cacheTime + 2 * 1000;
+    const cacheTime = 1492180435000
+    const now = cacheTime + 2 * 1000
 
     // prettier-ignore
     return [
@@ -31,13 +31,13 @@ test(
       [[now, { zen: 'foo', time: cacheTime }], 'foo']
     ]
   })
-);
+)
 
 test(
   'zen() should cache miss if cache is older than 5 seconds, GET zen from github, set cache, and return result',
   testZen(() => {
-    const cacheTime = 1492180435000;
-    const now = cacheTime + 10 * 1000;
+    const cacheTime = 1492180435000
+    const now = cacheTime + 10 * 1000
 
     // prettier-ignore
     return [
@@ -47,12 +47,12 @@ test(
       [null, 'foo']
     ]
   })
-);
+)
 
 test(
   'zen() should handle http GET failure',
   testZen(() => {
-    const now = 1492180435000;
+    const now = 1492180435000
 
     // prettier-ignore
     return [
@@ -61,4 +61,4 @@ test(
       [failure('oops'), failure('oops')]
     ]
   })
-);
+)
